a6d974960f9111aed6f73f806c2568346c4f075f
Arman-ATI <84268732+Arman-ATI@users.noreply.github.com>
2024-07-13 16:04:19 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,7 +33,10 @@ while true; do
             git clone https://github.com/akhilnarang/scripts
             cd scripts
             ./setup/android_build_env.sh
-            sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev nginx git-lfs -y
+            sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+            sudo apt install nginx -y
+            sudo apt install jq -y
+            sudo apt install git-lfs -y
             echo ""
             echo -e "Press ${RED}ENTER${NC} to continue"
             read -s -n 1
@@ -69,6 +72,9 @@ while true; do
             cd scripts
             ./setup/android_build_env.sh
             sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+            sudo apt install nginx -y
+            sudo apt install jq -y
+            sudo apt install git-lfs -y
             clear
             echo -e "${GREEN}installing swap${NC}" 
             echo ""
